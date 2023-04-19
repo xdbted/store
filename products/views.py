d82bb373056088b1f313a67e4f1586d84802f48e
xdbted <xdbted@gmail.com>
2023-04-19 20:38:15 +0300

    1. Создать модели в проекте Product и ProductCategory (обязательно должно быть поле с изображениями) и выполнить миграции 2. Поработать с моделями в консоли 3. Создать суперпользователя. Настроить админку и поработать в ней 4. Организовать работу с моделями в контроллерах и html-шаблонах 5. Настроить проект для работы с медиафайлами 6. Создать диспетчер URL-адресов в приложении. Скорректировать динамические URL-адреса в шаблонах 7. Организовать загрузку данных в базу из файла (Django fixtures)

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 
 # Create your views here.
 def index(request):
     context = {
-        'title' : 'Store'
+        'title': 'Store'
     }
     return render(request, 'products/index.html', context)
 
@@ -12,43 +13,7 @@ def index(request):
 def product(request):
     context = {
         'title': 'Store-Каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090.00,
-                'discription': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': 23725.00,
-                'discription': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390.00,
-                'discription': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-            },
-            {
-                'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
-                'name': 'Черный рюкзак Nike Heritage',
-                'price': 2340.00,
-                'discription': 'Плотная ткань. Легкий материал.',
-            },
-            {
-                'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
-                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'price': 13590.00,
-                'discription': 'Гладкий кожаный верх. Натуральный материал.',
-            },
-            {
-                'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'price': 2890.00,
-                'discription': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-            },
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
