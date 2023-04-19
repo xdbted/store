23be89f8bcdb3e0b6a995b52c5084b41945f8c9a
xdbted <xdbted@gmail.com>
2023-04-19 20:02:11 +0300

    1. Создать модели в проекте Product и ProductCategory (обязательно должно быть поле с изображениями) и выполнить миграции 2. Поработать с моделями в консоли

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=65)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    image = models.ImageField(upload_to='product_image')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
